799e465ed1b2777b8f02a8c6d7a09d380ed83f96
Puvvada Naga Sai Siva Sankar <om.ss.sankar@gmail.com>
2024-01-28 23:59:20 +0530

    added tickets assignment model

--- tickets/models.py
+++ tickets/models.py
@@ -78,8 +78,14 @@ class Ticket(models.Model):
     ticket_type = models.CharField(max_length=2, choices=TicketType.choices, default=TicketType.FEATURE)
     deleted = models.CharField(max_length=1, choices=Deleted.choices, default=Deleted.NO) # we will keep the user deleted tickets as well.
     segment = models.ForeignKey(segment_models.Segment, related_name='ticket_segment', on_delete=models.SET_NULL, null=True, blank=True) # segment is a sub part of the project. this field represents that this ticket belongs to which segment.
-    assigned_by = models.ManyToManyField(User, related_name='ticket_assigned_by', blank=True) # anybody can assign task to anybody if they have proper permissions.
-    assigned_to = models.ManyToManyField(User, related_name='ticket_assigned_to', blank=True) # anybody can get ticket assigned. if they are not present in database, an invitation has to be sent to their company mail.
+    
+    # anybody can assign task to anybody if they have proper permissions.
+    assigned_by = models.ManyToManyField(User, through='TicketAssignment', related_name='ticket_assigned_by', through_fields=('ticket', 'assigned_by'), blank=True)
+    
+    # anybody can get ticket assigned. if they are not present in database, an invitation has to be sent to their company mail.
+    assigned_to = models.ManyToManyField(User, through='TicketAssignment', related_name='ticket_assigned_to', through_fields=('ticket', 'assigned_to'), blank=True)
+
+   
     members = models.ManyToManyField(User, related_name='ticket_members', blank=True) # represent all members belongs to this ticket. Note that we exclude the members of sub and super tickets.
     tags = models.ManyToManyField(tag_models.Tag, related_name='ticket_tags', blank=True) # tags might be skills, teams, project names etc.., Basically what the user want to categorize, for the search purpose.
     stages = models.ManyToManyField(stage_models.Stage, through='TicketStage') # at which stage the ticket is in. For ex: Newly Created, Assigned, Blocked or On Hold, In progress, Testing, Review, Approval, Completed, Closed, Reopened and Canceled.
@@ -108,6 +114,48 @@ class Ticket(models.Model):
     def __str__(self):
         return f"#{self.company_ticket_counter}"
     
+
+
+class TicketAssignment(models.Model):
+    """
+    # Example usage in a view or serializer
+    ticket_instance = Project.objects.get(pk=1)
+
+    # Historical assignments given by ticket_instance.assigned_by
+    assignments_given = ticket_instance.assigned_by.all()
+
+    # Historical assignments received by ticket_instance.assigned_to
+    assignments_received = ticket_instance.assigned_to.all()
+    
+    from .models import Assignment
+
+    # Get the latest assignment for a particular ticket
+    latest_assignment = Assignment.objects.filter(ticket=ticket_instance).latest('assigned_at')
+    
+    from django.core.exceptions import ObjectDoesNotExist
+
+    try:
+        latest_assignment = Assignment.objects.filter(ticket=ticket_instance).latest('assigned_at')
+    except ObjectDoesNotExist:
+        # Handle the case where no assignments exist for the ticket
+        latest_assignment = None
+    """
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    assigned_by = models.ForeignKey(User, related_name='ticket_assignment_given_by', on_delete=models.SET_NULL, null=True, blank=True)
+    assigned_to = models.ForeignKey(User, related_name='ticket_assignment_received_to', on_delete=models.SET_NULL, null=True, blank=True)
+    assigned_at = models.DateTimeField(auto_now_add=True)
+    history = HistoricalRecords()
+    deleted = models.BooleanField(default=False)  # New field to mark soft-deleted records
+    created_by = models.ForeignKey(User, related_name='ticket_assignment_created_by', on_delete=models.SET_NULL, null=True, blank=True)
+    updated_by = models.ForeignKey(User, related_name='ticket_assignment_updated_by', on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+   
+    def __str__(self):
+        sentence = "#" + self.ticket.company_ticket_counter
+        return sentence
+     
+
 class TicketStage(models.Model):
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
     stage = models.ForeignKey(stage_models.Stage, on_delete=models.CASCADE)
